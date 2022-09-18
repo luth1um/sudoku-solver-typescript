ac734f3dd4150d7779af0220441a1bee89ff5352
luth1um <52032867+luth1um@users.noreply.github.com>
2022-09-18 11:12:05 +0200

    added code for solver

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export function hello() {
-  console.log("hello world");
+import { solveSudokuPuzzle } from './sudoku-solver';
+
+/**
+ * Solves Sudoku puzzles. The first entry of the returned array is *false* if the Sudoku puzzle is not solvable.
+ * @param sudoku Sudoku puzzle to solve
+ * @returns solved Sudoku puzzle. The first entry of the returned array is *false* if the Sudoku puzzle is not solvable.
+ */
+export function solveSudoku(sudoku: number[][]): [solvable: boolean, solution: number[][]] {
+  return solveSudokuPuzzle(sudoku);
 }
