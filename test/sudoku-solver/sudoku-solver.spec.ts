b460ba47cbbc8d659240a78185a230454be04e86
luth1um <52032867+luth1um@users.noreply.github.com>
2022-10-03 17:11:28 +0200

    fixed test cases

--- test/sudoku-solver/sudoku-solver.spec.ts
+++ test/sudoku-solver/sudoku-solver.spec.ts
@@ -2,7 +2,7 @@ import { solveSudokuPuzzle } from '../../src/sudoku-solver/sudoku-solver';
 
 test('Sudoku solver should solve a valid Sudoku puzzle correctly', () => {
   const sudoku: number[][] = [
-    [-1, -1, -1, -1, -1, 4, -1, 9],
+    [-1, -1, -1, -1, -1, 4, -1, 9, -1],
     [8, -1, 2, 9, 7, -1, -1, -1, -1],
     [9, -1, 1, 2, -1, -1, 3, -1, -1],
     [-1, -1, -1, -1, 4, 9, 1, 5, 7],
