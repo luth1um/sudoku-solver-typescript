01b39624fb0fbf9c12be84361ef9747ef12c7624
luth1um <52032867+luth1um@users.noreply.github.com>
2024-05-30 16:55:14 +0200

    added Prettier for formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,15 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended);
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
+  {
+    // needs to be in its own object to act as global ignore
+    ignores: ['dist', '*.cjs'],
+  }
+);
