5f7c57fa8bca456626167dbd971cef000b04e520
luth1um <52032867+luth1um@users.noreply.github.com>
2025-11-07 21:48:44 +0100

    removed unnecessary dependency

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,8 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
-  eslintConfigPrettier,
-  {
-    // needs to be in its own object to act as global ignore
-    ignores: ['dist', '*.cjs'],
-  }
-);
+export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier, {
+  // needs to be in its own object to act as global ignore
+  ignores: ['dist', '*.cjs'],
+});
