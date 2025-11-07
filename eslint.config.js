a5f6cd5ee5ce9f25dc85cdc2e68a6211662a6b51
luth1um <52032867+luth1um@users.noreply.github.com>
2025-11-07 22:01:51 +0100

    updated config to use defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js';
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier, {
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, eslintConfigPrettier, {
   // needs to be in its own object to act as global ignore
   ignores: ['dist', '*.cjs'],
 });
