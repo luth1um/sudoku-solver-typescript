45ae70c2d27fe5a63780a2db51cd2175f5caa1a2
luth1um <52032867+luth1um@users.noreply.github.com>
2026-01-01 12:28:53 +0100

    refactored tests for improved readability and maintainability

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,75 +1,48 @@
 import { expect, test } from 'vitest';
-import { nextNumber, solveSudoku } from '../src/index';
+import { nextNumber, solveSudoku } from '../src';
 import { solveNextNumber } from '../src/sudoku-solver/next-number-solver';
 import { cloneSudoku } from '../src/sudoku-solver/solver-utils';
+import { createSolutionForSolvableTestSudoku, createSolvableTestSudoku } from './test-utils';
 
 test('The solver function in index.ts should call the internal solver and return the correct result (e2e test)', () => {
-  const sudoku: number[][] = [
-    [-1, -1, -1, -1, -1, 4, -1, 9],
-    [8, -1, 2, 9, 7, -1, -1, -1, -1],
-    [9, -1, 1, 2, -1, -1, 3, -1, -1],
-    [-1, -1, -1, -1, 4, 9, 1, 5, 7],
-    [-1, 1, 3, -1, 5, -1, 9, 2, -1],
-    [5, 7, 9, 1, 2, -1, -1, -1, -1],
-    [-1, -1, 7, -1, -1, 2, 6, -1, 3],
-    [-1, -1, -1, -1, 3, 8, 2, -1, 5],
-    [-1, 2, -1, 5, -1, -1, -1, -1, -1],
-  ];
-  const correctSolution: number[][] = [
-    [7, 3, 5, 6, 1, 4, 8, 9, 2],
-    [8, 4, 2, 9, 7, 3, 5, 6, 1],
-    [9, 6, 1, 2, 8, 5, 3, 7, 4],
-    [2, 8, 6, 3, 4, 9, 1, 5, 7],
-    [4, 1, 3, 8, 5, 7, 9, 2, 6],
-    [5, 7, 9, 1, 2, 6, 4, 3, 8],
-    [1, 5, 7, 4, 9, 2, 6, 8, 3],
-    [6, 9, 4, 7, 3, 8, 2, 1, 5],
-    [3, 2, 8, 5, 6, 1, 7, 4, 9],
-  ];
+  // given
+  const sudoku = createSolvableTestSudoku();
+  const correctSolution = createSolutionForSolvableTestSudoku();
+
+  // when
   const result = solveSudoku(sudoku);
-  expect(result[0]).toEqual(true);
+
+  // then
+  expect(result[0]).toBe(true);
   expect(result[1]).toEqual(correctSolution);
 });
 
 test('The next-number solver in index.ts should call the internal solver and return the same result (e2e test)', () => {
-  const sudoku: number[][] = [
-    [-1, -1, -1, -1, -1, 4, -1, 9],
-    [8, -1, 2, 9, 7, -1, -1, -1, -1],
-    [9, -1, 1, 2, -1, -1, 3, -1, -1],
-    [-1, -1, -1, -1, 4, 9, 1, 5, 7],
-    [-1, 1, 3, -1, 5, -1, 9, 2, -1],
-    [5, 7, 9, 1, 2, -1, -1, -1, -1],
-    [-1, -1, 7, -1, -1, 2, 6, -1, 3],
-    [-1, -1, -1, -1, 3, 8, 2, -1, 5],
-    [-1, 2, -1, 5, -1, -1, -1, -1, -1],
-  ];
+  // given
+  const sudoku = createSolvableTestSudoku();
+
+  // when
   const resultIndex = nextNumber(sudoku);
   const resultInternal = solveNextNumber(sudoku);
-  expect(resultIndex.isSolvable).toEqual(resultInternal.isSolvable);
-  expect(resultIndex.row).toEqual(resultInternal.row);
-  expect(resultIndex.column).toEqual(resultInternal.column);
-  expect(resultIndex.entry).toEqual(resultInternal.entry);
+
+  // then
+  expect(resultIndex.isSolvable).toBe(resultInternal.isSolvable);
+  expect(resultIndex.row).toBe(resultInternal.row);
+  expect(resultIndex.column).toBe(resultInternal.column);
+  expect(resultIndex.entry).toBe(resultInternal.entry);
 });
 
 test('Repeatedly calling the next-number solver should eventually give the same result as calling the Sudoku solver once (e2e test)', () => {
-  const sudoku: number[][] = [
-    [-1, -1, -1, -1, -1, 4, -1, 9],
-    [8, -1, 2, 9, 7, -1, -1, -1, -1],
-    [9, -1, 1, 2, -1, -1, 3, -1, -1],
-    [-1, -1, -1, -1, 4, 9, 1, 5, 7],
-    [-1, 1, 3, -1, 5, -1, 9, 2, -1],
-    [5, 7, 9, 1, 2, -1, -1, -1, -1],
-    [-1, -1, 7, -1, -1, 2, 6, -1, 3],
-    [-1, -1, -1, -1, 3, 8, 2, -1, 5],
-    [-1, 2, -1, 5, -1, -1, -1, -1, -1],
-  ];
+  // given
+  const sudoku = createSolvableTestSudoku();
   const cloneNextNumber = cloneSudoku(sudoku);
 
+  // when / then
   const resultSolveAtOnce = solveSudoku(sudoku);
 
   while (someEntriesLeft(cloneNextNumber)) {
     const nextEntry = solveNextNumber(cloneNextNumber);
-    expect(nextEntry.isSolvable).toEqual(true);
+    expect(nextEntry.isSolvable).toBe(true);
     cloneNextNumber[nextEntry.row][nextEntry.column] = nextEntry.entry;
   }
 
--- test/sudoku-solver/next-number-solver.spec.ts
+++ test/sudoku-solver/next-number-solver.spec.ts
@@ -1,106 +1,74 @@
 import { expect, test } from 'vitest';
 import { solveNextNumber } from '../../src/sudoku-solver/next-number-solver';
+import {
+  createEmptyTestSudoku,
+  createSolutionForSolvableTestSudoku,
+  createSolvableTestSudoku,
+  createUnsolvableTestSudoku,
+} from '../test-utils';
 
 test('The next-number solver should return the correct next number', () => {
-  const sudoku: number[][] = [
-    [-1, -1, -1, -1, -1, 4, -1, 9, -1],
-    [8, -1, 2, 9, 7, -1, -1, -1, -1],
-    [9, -1, 1, 2, -1, -1, 3, -1, -1],
-    [-1, -1, -1, -1, 4, 9, 1, 5, 7],
-    [-1, 1, 3, -1, 5, -1, 9, 2, -1],
-    [5, 7, 9, 1, 2, -1, -1, -1, -1],
-    [-1, -1, 7, -1, -1, 2, 6, -1, 3],
-    [-1, -1, -1, -1, 3, 8, 2, -1, 5],
-    [-1, 2, -1, 5, -1, -1, -1, -1, -1],
-  ];
+  // given
+  const sudoku = createSolvableTestSudoku();
+
+  // when
   const result = solveNextNumber(sudoku);
-  expect(result.isSolvable).toEqual(true);
-  expect(result.row).toEqual(6);
-  expect(result.column).toEqual(3);
-  expect(result.entry).toEqual(4);
+
+  // then
+  expect(result.isSolvable).toBe(true);
+  expect(result.row).toBe(6);
+  expect(result.column).toBe(3);
+  expect(result.entry).toBe(4);
 });
 
 test('The next-number solver should mark an unsolvable Sudoku puzzle as unsolvable', () => {
-  const unsolvableSudoku: number[][] = [
-    [-1, -1, -1, -1, -1, -1, 4, 8, 9],
-    [-1, -1, -1, -1, -1, -1, 5, -1, 3],
-    [-1, -1, -1, -1, 3, -1, 6, -1, 2],
-    [-1, -1, -1, -1, -1, -1, 2, 7, 4],
-    [-1, -1, -1, -1, -1, -1, 8, 9, 5],
-    [-1, 2, -1, -1, -1, -1, 1, 3, 6],
-    [-1, -1, -1, -1, -1, -1, -1, -1, 7],
-    [-1, -1, -1, -1, -1, -1, -1, -1, 8],
-    [-1, -1, -1, -1, -1, -1, -1, -1, 1],
-  ];
+  // given
+  const unsolvableSudoku = createUnsolvableTestSudoku();
+
+  // when
   const result = solveNextNumber(unsolvableSudoku);
-  expect(result.isSolvable).toEqual(false);
+
+  // then
+  expect(result.isSolvable).toBe(false);
 });
 
 test('The next-number solver should calculate the first number for the empty Sudoku puzzle correctly', () => {
-  const emptySudoku: number[][] = [
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-  ];
+  // given
+  const emptySudoku = createEmptyTestSudoku();
+
+  // when
   const result = solveNextNumber(emptySudoku);
-  expect(result.isSolvable).toEqual(true);
-  expect(result.row).toEqual(0);
-  expect(result.column).toEqual(0);
-  expect(result.entry).toEqual(1);
+
+  // then
+  expect(result.isSolvable).toBe(true);
+  expect(result.row).toBe(0);
+  expect(result.column).toBe(0);
+  expect(result.entry).toBe(1);
 });
 
 test('The next-number solver should return {true, -1, -1, -1} in case the input is already completed', () => {
-  const sudoku: number[][] = [
-    [7, 3, 5, 6, 1, 4, 8, 9, 2],
-    [8, 4, 2, 9, 7, 3, 5, 6, 1],
-    [9, 6, 1, 2, 8, 5, 3, 7, 4],
-    [2, 8, 6, 3, 4, 9, 1, 5, 7],
-    [4, 1, 3, 8, 5, 7, 9, 2, 6],
-    [5, 7, 9, 1, 2, 6, 4, 3, 8],
-    [1, 5, 7, 4, 9, 2, 6, 8, 3],
-    [6, 9, 4, 7, 3, 8, 2, 1, 5],
-    [3, 2, 8, 5, 6, 1, 7, 4, 9],
-  ];
+  // given
+  const sudoku = createSolutionForSolvableTestSudoku();
+
+  // when
   const result = solveNextNumber(sudoku);
-  expect(result.isSolvable).toEqual(true);
-  expect(result.row).toEqual(-1);
-  expect(result.column).toEqual(-1);
-  expect(result.entry).toEqual(-1);
+
+  // then
+  expect(result.isSolvable).toBe(true);
+  expect(result.row).toBe(-1);
+  expect(result.column).toBe(-1);
+  expect(result.entry).toBe(-1);
 });
 
 test('Repeated calls to the next-number solver should eventually result in a correctly and completely solved Sudoku puzzle', () => {
-  const sudoku: number[][] = [
-    [-1, -1, -1, -1, -1, 4, -1, 9],
-    [8, -1, 2, 9, 7, -1, -1, -1, -1],
-    [9, -1, 1, 2, -1, -1, 3, -1, -1],
-    [-1, -1, -1, -1, 4, 9, 1, 5, 7],
-    [-1, 1, 3, -1, 5, -1, 9, 2, -1],
-    [5, 7, 9, 1, 2, -1, -1, -1, -1],
-    [-1, -1, 7, -1, -1, 2, 6, -1, 3],
-    [-1, -1, -1, -1, 3, 8, 2, -1, 5],
-    [-1, 2, -1, 5, -1, -1, -1, -1, -1],
-  ];
-  const correctSolution: number[][] = [
-    [7, 3, 5, 6, 1, 4, 8, 9, 2],
-    [8, 4, 2, 9, 7, 3, 5, 6, 1],
-    [9, 6, 1, 2, 8, 5, 3, 7, 4],
-    [2, 8, 6, 3, 4, 9, 1, 5, 7],
-    [4, 1, 3, 8, 5, 7, 9, 2, 6],
-    [5, 7, 9, 1, 2, 6, 4, 3, 8],
-    [1, 5, 7, 4, 9, 2, 6, 8, 3],
-    [6, 9, 4, 7, 3, 8, 2, 1, 5],
-    [3, 2, 8, 5, 6, 1, 7, 4, 9],
-  ];
+  // given
+  const sudoku = createSolvableTestSudoku();
+  const correctSolution = createSolutionForSolvableTestSudoku();
 
+  // when / then
   while (someEntriesLeft(sudoku)) {
     const nextEntry = solveNextNumber(sudoku);
-    expect(nextEntry.isSolvable).toEqual(true);
+    expect(nextEntry.isSolvable).toBe(true);
     sudoku[nextEntry.row][nextEntry.column] = nextEntry.entry;
   }
 
